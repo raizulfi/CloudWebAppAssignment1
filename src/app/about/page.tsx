7bcad18852e43e3a10eb6fbf9a31a5d162e0422d
Raihan Zulfi <114371959+raizulfi@users.noreply.github.com>
2025-10-06 20:38:17 +0700

    Creating basic layout and links pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">About Me</h1>
+      <p>Name: Your Name</p>
+      <p>Student Number: 123456</p>
+
+      <div className="mt-4">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="Demo Video"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
